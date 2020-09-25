8ad82e60c94dd7eb3467231973cb593468f5d49a
AlexLeo <leontev,mmaker@gmail.com>
2020-09-25 17:45:54 +0300

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+const sass = require('node-sass');
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'src/css/main.css': 'src/css/main.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+    grunt.registerTask('default', ['sass', 'watch']);
+}
